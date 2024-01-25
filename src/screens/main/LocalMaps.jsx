a2a55a4dd7c7b86c42a7684234c2f0643d84bb8f
GiftSibiya <sibiya.bobo@gmail.com>
2024-01-25 21:02:33 +0200

    ADDED MAP VIEW

--- src/screens/main/LocalMaps.jsx
+++ src/screens/main/LocalMaps.jsx
@@ -1,12 +1,26 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import MapView from "react-native-maps";
 
 const LocalMaps = () => {
   return (
-    <View>
-      <Text>LocalMaps</Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: -26.007073,
+          longitude: 28.183106,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 };
 
 export default LocalMaps;
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  map: { width: "100%", height: "100%" },
+});
